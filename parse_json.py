79514d3b911b1c20c320f3929135d90b1d84b1bd
Dusan Vilicic Held <dvheld@gmail.com>
2025-03-21 12:41:04 -0300

    Finished parsing script

--- parse_json.py
+++ parse_json.py
@@ -5,47 +5,52 @@ from pathlib import Path
 from json import loads
 
 def parse_fighters() -> bool:
+    """Parses the input fighters.json file into a correctly formatted csv.
 
-    full_input_path = Path(__file__).parent / 'data' / 'fighters.json'
-    with open(full_input_path, "r", encoding="utf-8") as input_file:
+    :return bool: True when the parsing and writing finishes.
+    """
+
+    file_path = Path(__file__).parent / 'data' / 'fighters.json'
+    with open(file_path, "r", encoding="utf-8") as input_file:
         downloaded_data = loads(input_file.read())
-
-    full_output_path = Path(__file__).parent / 'data' / 'fighters.csv'
-    if path.exists(full_output_path):
+    file_path = Path(__file__).parent / 'data' / 'fighters.csv'
+    if path.exists(file_path):
         mode = 'r+'
     else:
         mode = "w+"
-    print(mode)
-    with open(full_output_path, mode, encoding='utf-8-sig', newline='') as output_file:
+    with open(file_path, mode, encoding='utf-8-sig', newline='') as output_file:
 
         # Existing fighters to skip
         if mode == "r+":
             to_skip = []
-            print(output_file.readline())
-            for fighter in output_file.readline():
+            output_file.readline()
+            fighter = output_file.readline()
+            while fighter:
                 print(fighter)
                 to_skip.append(fighter.split(";")[1])
+                fighter = output_file.readline()
 
+        # Get column header names for synching
         header_names = []
         for column_header in list(downloaded_data.values())[0].keys():
             header_names.append(column_header)
 
-        # Adding column headers to newly created file
-        if mode == "w+":
-            print("a")
-            column_headers = "fighter_id;fighter_alternate_id"
-            for header_name in header_names:
-                column_headers += ";" + header_name
-            print("b")
-            output_file.write(column_headers + "\n")
-            print("c")
-
         fighter_id = len(output_file.readlines())
         output_file.seek(0)
 
+        # Adding column headers to newly created file
+        if mode == "w+":
+            column_headers = "fighter_id;fighter_alternate_id"
+            for header_name in header_names:
+                column_headers += ";" + header_name
+            output_file.write(column_headers + "\n")
+
+        # Write each fighter's data into the output file
         for fighter_alternate_id, fighter_data in downloaded_data.items():
             position = 0
             record = str(fighter_id) + ";" + fighter_alternate_id
+
+            # Synch for empty data
             for stat, value in fighter_data.items():
                 while stat != header_names[position]:
                     record += ";"
@@ -55,11 +60,4 @@ def parse_fighters() -> bool:
 
             output_file.write(record + "\n")
             fighter_id += 1
-            # print(record)
-        
-            
-            
-    
-    return False
-
-parse_fighters()
+    return True
