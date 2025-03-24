c7326922f63e948f595d9dc0e1fdc5fa9e9f0807
Dusan Vilicic Held <dvheld@gmail.com>
2025-03-24 20:41:15 -0300

    Improvements
    * Added cleanup when adding the alternate ids.
    * Standardized style names.

--- create_id.py
+++ create_id.py
@@ -3,9 +3,16 @@
 from pathlib import Path
 from csv import reader
 from unicodedata import normalize, combining
+from string import ascii_letters
 
 def _remove_accents(text: str) -> str:
     return ''.join([char for char in normalize('NFKD', text) if not combining(char)])
+
+def _cleanup(text: str) -> str:
+    allowed_characters = ascii_letters + '-'
+    translation_table = {ord(char): None for char in ''.join(set(text)) if char not in allowed_characters}
+    return text.translate(translation_table)
+
 
 def create_fight_id() -> bool:
     """Parses the input fights.csv file to add the ID columns to it.
@@ -30,10 +37,27 @@ def create_fight_id() -> bool:
                 record = str(fight_id) + ";" + row[6].replace("-", "") + "-" +\
                          _remove_accents(row[0]).lower().replace(" ", "_") + "-" +\
                          _remove_accents(row[1]).lower().replace(" ", "_")
-                record += ";" + _remove_accents(row[0]).lower().replace(" ", "-")
-                record += ";" + _remove_accents(row[1]).lower().replace(" ", "-")
+                record += ";" + _cleanup(_remove_accents(row[0]).lower().replace(" ", "-"))
+                record += ";" + _cleanup(_remove_accents(row[1]).lower().replace(" ", "-"))
                 for stat in row:
-                    record += ";" + _remove_accents(stat).replace(".", ",")
+                    normalized_stat = _remove_accents(stat).replace(".", ",")
+                    if normalized_stat == "Wrestler":
+                        normalized_stat = "Wrestling"
+                    elif normalized_stat == "Kickboxer":
+                        normalized_stat = "Kickboxing"
+                    elif normalized_stat == "Boxer":
+                        normalized_stat = "Boxing"
+                    elif normalized_stat == "":
+                        normalized_stat = "N/E"
+                    elif normalized_stat == "NULL":
+                        normalized_stat = "N/E"
+                    elif normalized_stat == "Brawler":
+                        normalized_stat = "Brawling"
+                    elif normalized_stat == "Grappler":
+                        normalized_stat = "Grappling"
+                    elif normalized_stat == "Striker":
+                        normalized_stat = "Striking"
+                    record += ";" + normalized_stat
                 output_file.write(record + "\n")
                 fight_id += 1
     return True
